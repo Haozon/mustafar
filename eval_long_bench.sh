9e6790a6fbc7112c08e511ca9a8a3661a6a44765
zhanghao <2410025784@qq.com>
2026-03-16 16:41:34 +0800

    解决远程代码冲突，合并最新修改

--- eval_long_bench.sh
+++
@@ -1,9 +0,0 @@
-# python3 eval_long_bench.py --model llama-2-7b_4096_K_0.5_V_0.5 &
-# python3 eval_long_bench.py --model llama-2-7b_4096_K_0.7_V_0.7
-# python3 eval_long_bench.py --model llama-2-7b_4096_K_0.0_V_0.0
-# python3 eval_long_bench.py --model Meta-Llama-3-8B-Instruct_8192_K_0.0_V_0.0
-# python3 eval_long_bench.py --model Meta-Llama-3-8B-Instruct_8192_K_0.7_V_0.7
-# python3 eval_long_bench.py --model pred_greedy/Meta-Llama-3-8B-Instruct_8192_greedy_avg_0.700
-# python3 eval_long_bench.py --model pred_fisher/Meta-Llama-3-8B-Instruct_8192_fisher_avg_0.700
-# python3 eval_long_bench.py --model pred_custom/Meta-Llama-3-8B-Instruct_8192_greedy_avg_0.702
-python3 eval_long_bench.py --model pred_same/Meta-Llama-3-8B-Instruct_8192_greedy_avg_0.700
--- nsys.sh
+++
@@ -1,2 +0,0 @@
-nsys profile --stats=true -o mustafar python3 spd_test.py
-# nsys profile --stats=true -o naive python3 spd_test.py
